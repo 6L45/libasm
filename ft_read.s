71b3990931d389a460799a67c81251c8835f6fc0
6L45 <110972601+6L45@users.noreply.github.com>
2023-09-29 23:18:24 +0200

    Update ft_read.s

--- ft_read.s
+++ ft_read.s
@@ -9,17 +9,17 @@ ft_read:
 	mov	rbp, rsp
 	; ------------------------
 
-	mov	rax, 0	; set rax for sys_read
+	mov	rax, 0				; set rax for sys_read
 	syscall
 
-	cmp	rax, 0	; if (rax == 0)
-	jge	quit	;	goto(quit)
+	cmp	rax, 0				; if (rax == 0)
+	jge	quit				;	goto(quit)
 
-	neg	rax		; rax *= -1
+	neg	rax				; rax *= -1
 	mov	rdi, rax
 	call __errno_location	WRT ..plt	; return a pointer to errno in rax
 
-	mov	[rax], rdi	; *errno = rdi
+	mov	[rax], rdi			; *errno = rdi
 	mov	rax, -0x1
 
 	quit:
