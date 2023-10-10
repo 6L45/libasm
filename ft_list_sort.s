7c86e60dbc6743dfe5d71f20e14db46215c1a5f8
6L45 <silas.dbr170@gmail.com>
2023-10-10 21:42:31 +0200

    ft_list_sort.s += comments

--- ft_list_sort.s
+++ ft_list_sort.s
@@ -20,28 +20,28 @@ ft_list_sort:
 	; r10			=>	rsi	
 	; r12	rdi->next
 	loop:
-		mov	r12, [r8 + 0x8]		; while (list->next)
-		cmp	r12, 0x0		; {
-		je	quit			;
+		mov	r12, [r8 + 0x8]			; while (list->next)
+		cmp	r12, 0x0			; {
+		je	quit				;
 
-		mov	rdi, [r8]		;	|
-		mov	rsi, [r12]		;	| rax = strcmp(*r8, *r12)
-		call	r10			;	|	strcmp(list->data, list->next->data)
-		cmp	eax, 0x0		;	if (rax < 0)
-		jg	swap_data		;
+		mov	rdi, [r8]			;	|
+		mov	rsi, [r12]			;	| rax = strcmp(*r8, *r12)
+		call	r10				;	|	// strcmp(list->data, list->next->data)
+		cmp	eax, 0x0			;	if (rax < 0)
+		jg	swap_data			;
 
-		mov	r8, [r8 + 0x8]		;		r8 = r8->next
+		mov	r8, [r8 + 0x8]			;		r8 = r8->next
 		jmp	loop
 
-		swap_data:				; else {
-			mov	rax, [r8]		;	tmp = r8->data
+		swap_data:				;	  else {
+			mov	rax, [r8]		;		tmp = r8->data
 			mov	rcx, [r12]		;
-			mov	[r8], rcx		;	r8->data = r12->data
-			mov	[r12], rax		;	r12->data = tmp
+			mov	[r8], rcx		;		r8->data = r12->data
+			mov	[r12], rax		;		r12->data = tmp
 
 			mov	r8, r14			;
-							; }
-			jmp	loop		; }
+							;	  }
+			jmp	loop			; }
 
 	quit:
 		leave
