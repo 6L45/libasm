f140c86711215aec9adefb11c328973e33d3dc85
6L45 <silas.dbr170@gmail.com>
2023-10-11 14:12:22 +0200

    bck up main_list before main bonus final

--- main_list.c
+++ main_list.c
@@ -11,8 +11,42 @@ typedef struct	s_list
 void	ft_list_push_front(t_list **alst, void *data);
 int	ft_list_size(t_list *lst);
 int	ft_list_sort(t_list **begin_list, int (*cmp)());
-//void	ft_list_remove_if(t_list **begin_list, void *data_ref,
-				//int (*cmp)(), void (*free_fct)(void *));
+void	ft_list_remove_if(t_list **begin_list, void *data_ref,
+				int (*cmp)(), void (*free_fct)(void *));
+
+void	rmv_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *))
+{
+	if (!begin_list || !(*begin_list) || !data_ref || !cmp || !free_fct)
+		return;
+
+	t_list	*iterator = *begin_list;
+	t_list	*prev = NULL;
+	while(iterator)
+	{
+		if ((cmp(iterator->data, data_ref)) == 0)
+		{
+			if (!prev)
+			{
+				*begin_list = iterator->next;
+				free_fct(iterator);
+				iterator = *begin_list;
+			}
+			else
+			{
+				prev->next = iterator->next;
+				free_fct(iterator);
+				iterator = prev->next;
+			}
+		}
+		else
+		{
+			prev = iterator;
+			iterator = iterator->next;
+		}
+	}
+
+}
+
 
 int	main(int argc, char **argv)
 {
@@ -34,9 +68,9 @@ int	main(int argc, char **argv)
 			tmp = tmp->next;
 		}
 	}
-
-	ft_list_sort(&lst, strcmp);
-	printf("\n");
+	
+	ft_list_remove_if(&lst, "math", strcmp, free);
+	printf("\n\n");
 
 	{
 		t_list	*tmp = lst;
